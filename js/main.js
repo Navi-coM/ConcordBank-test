fe0c2f43d45f4881ca498acad1bcd3069dff111b
Ivan <31728037+Navi-coM@users.noreply.github.com>
2019-07-29 17:59:10 +0300

    Заливка картинок и js файлов
    Залил в репозиторий картинки и js файлы

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//---  Подгрузка картинок (Lazy Load)---------------
+
+$(function() {
+    $('.carousel.lazy-load').bind('slide.bs.carousel', 
+    function (e) {
+        var image = $(e.relatedTarget).find('img[data-src]');
+        image.attr('src', image.data('src'));
+        image.removeAttr('data-src');
+    });
+});
+
+
+// ------- Настройки каруселей  ------------
+$('#carouselExampleControls, #carouselExampleControls2').carousel({
+  interval: 4000,
+  wrap: false
+});
+
+
+// --------- Аккордеон  ---------
